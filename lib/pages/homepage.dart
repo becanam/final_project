85ad1f5daefc45ed982ae0e426619380487f049f
junghyun <mattew423@gmail.com>
2023-12-24 02:11:29 +0900

    trying to update

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -14,45 +14,36 @@ class Homepage extends ConsumerStatefulWidget {
   _HomepageState createState() => _HomepageState();
 }
 
-class _HomePageState extends State<Homepage> {
-  //padding constants
-  final double horizontalPadding = 30;
-  final double verticalPadding = 20;
+class _HomepageState extends ConsumerState<Homepage> {
+  Future<void> fetchUserData() async {
+    var url = Uri.parse('http://localhost:3000/users');
+    try {
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        var users = json.decode(response.body) as List;
+        if (users.isNotEmpty) {
+          var currentUser = users.last['username'];
+          ref.read(userProvider.state).state = currentUser;
+        } else {
+          ref.read(userProvider.state).state = null;
+        }
+      } else {
+        print('Failed to fetch users: ${response.statusCode}, ${response.body}');
+      }
+    } catch (e) {
+      print('Error occurred while fetching users: $e');
+    }
+  }
 
-// list of places
-  List travelPlaces = [
-    [
-      "Chennai, India",
-      "lib/icons/flag.png",
-      "⭐️ 4.2 Rating",
-      "lib/icons/chennai.jpg"
-    ],
-    [
-      "Sapporo, Japan",
-      "lib/icons/japan.png",
-      "⭐️ 4.5 Rating",
-      "lib/icons/sapporo.jpg"
-    ],
-    [
-      "Barcelona, Spain",
-      "lib/icons/spain.png",
-      "⭐️ 4.8 Rating",
-      "lib/icons/barcelona.jpg"
-    ],
-    [
-      "Rio de Janeiro, Brazil",
-      "lib/icons/brazil.png",
-      "⭐️ 4.1 Rating",
-      "lib/icons/rio_de_janeiro.jpg"
-    ]
-  ];
-
-  void _navigateToDetailPage(BuildContext context, String countryName) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) => DetailPage(countryName: countryName),
-      ),
-    );
+  Future<List<dynamic>> fetchTravelPlans() async {
+    var url = Uri.parse('http://localhost:3000/travel_plans');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      print('Failed to fetch travel plans: ${response.statusCode}, ${response.body}');
+      return [];
+    }
   }
 
   @override
@@ -63,224 +54,205 @@ class _HomePageState extends State<Homepage> {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //appbar
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: horizontalPadding,
-                vertical: verticalPadding,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: verticalPadding),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.grey[300],
+                      radius: 25,
+                      child: Image.asset('lib/icons/app.png', height: horizontalPadding, color: Colors.grey[800]),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        await fetchUserData();
+                        if (username != null) {
+                          _showLogoutDialog(context, username);
+                        } else {
+                          _showLoginDialog(context);
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(15),
+                        primary: Colors.grey[300],
+                      ),
+                      child: Image.asset('lib/icons/user.png', height: horizontalPadding, color: Colors.grey[800]),
+                    ),
+                  ],
+                ),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  //menu icon
-                  CircleAvatar(
-                    backgroundColor: Colors.grey[300],
-                    radius: 25,
-                    child: Image.asset('lib/icons/app.png',
-                        height: horizontalPadding, color: Colors.grey[800]),
-                  ),
-
-                  //profile
-                  CircleAvatar(
-                    backgroundColor: Colors.grey[300],
-                    radius: 25,
-                    child: Image.asset('lib/icons/user.png',
-                        height: horizontalPadding, color: Colors.grey[800]),
-                  )
-                ],
-              ),
-            ),
-
-            //title
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: horizontalPadding, vertical: verticalPadding),
-              child: Container(
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: verticalPadding),
+                child: Container(
                   width: 250,
                   height: 125,
-                  color: Colors.black.withOpacity(0),
-                  child: const Column(
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        "Best Places For Travel",
-                        style: TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        "Find the best places to visit!",
-                        style: TextStyle(
-                            wordSpacing: 2,
-                            height: 2,
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 135, 131, 131)),
-                      ),
+                      Text("Best Places For Travel", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+                      Text("Find the best places to visit!", style: TextStyle(wordSpacing: 2, height: 2, fontSize: 14, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 135, 131, 131))),
                     ],
-                  )),
-            ),
-
-            //scrollable tab
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Center(child: CategoryTabs()),
-            ),
-
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: horizontalPadding, vertical: verticalPadding),
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  //trending now
-                  Text(
-                    "Trending Now",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
                   ),
-
-                  //see all
-                  Text(
-                    "See all",
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue,
-                    ),
-                  )
-                ],
-              ),
-            ),
-
-            //travel options
-            Expanded(
-              child: GridView.builder(
-                itemCount: travelPlaces.length,
-                padding: const EdgeInsets.all(16),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 1 / 1.65,
                 ),
-                itemBuilder: (context, index) {
-                  return InkWell(
-                    onTap: () {
-                      _navigateToDetailPage(context, travelPlaces[index][0]);
-                    },
-                    child: PlacesToTravel(
-                      countryName: travelPlaces[index][0],
-                      iconPath: travelPlaces[index][1],
-                      rating: travelPlaces[index][2],
-                      cityImage: travelPlaces[index][3],
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Center(child: CategoryTabs()),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: horizontalPadding, vertical: verticalPadding),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Travel Plan", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddTravelPlanPage()));
+                      },
+                      child: Text("Add Plan", style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+                      style: ElevatedButton.styleFrom(primary: Colors.blue, onPrimary: Colors.white),
                     ),
+                  ],
+                ),
+              ),
+              FutureBuilder<List<dynamic>>(
+                future: fetchTravelPlans(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20.0),
+                      child: Center(child: Text("No travel plans available.")),
+                    );
+                  }
+                  return GridView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: snapshot.data!.length,
+                    padding: const EdgeInsets.all(16),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1 / 1, // Adjusted childAspectRatio
+                    ),
+                    itemBuilder: (context, index) {
+                      var plan = snapshot.data![index];
+                      return Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Image.network(
+                              plan['imageUrl'] ?? 'assets/default_image.png',
+                              height: 250.0,
+                              fit: BoxFit.cover,
+                              errorBuilder: (context, error, stackTrace) {
+                                return Image.asset(
+                                  'assets/default_image.png',
+                                  height: 250.0,
+                                  fit: BoxFit.cover,
+                                );
+                              },
+                            ),
+                            SizedBox(height: 10), // Adjusted gap
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    plan['name'] ?? 'Unknown Name',
+                                    style: TextStyle(
+                                      fontSize: 24, // Increased font size
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  Text(
+                                    "국가: ${plan['country'] ?? 'Unknown'}",
+                                    style: TextStyle(fontSize: 18), // Increased font size
+                                    textAlign: TextAlign.center,
+                                  ),
+                                    Text(
+                                      "도시: ${plan['city'] ?? 'Unknown'}",
+                                      style: TextStyle(fontSize: 20),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    Text(
+                                      "시작일: ${plan['startDate'] ?? 'Unknown'}",
+                                      style: TextStyle(fontSize: 20),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    Text(
+                                      "종료일: ${plan['endDate'] ?? 'Unknown'}",
+                                      style: TextStyle(fontSize: 20),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    Text(
+                                      "가격(usd): ${plan['price']?.toString() ?? 'Unknown'}",
+                                      style: TextStyle(fontSize: 20),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    Text(
+                                      "테마: ${plan['theme'] ?? 'Unknown'}",
+                                      style: TextStyle(fontSize: 20),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                       ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
                   );
                 },
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
-}
 
-// DetailPage 클래스 정의
-
-class DetailPage extends StatefulWidget {
-  final String countryName;
-
-  const DetailPage({required this.countryName});
-
-  @override
-  _DetailPageState createState() => _DetailPageState();
-}
-
-class _DetailPageState extends State<DetailPage> {
-  double iconSize = 48.0;
-  bool isMaxSizeReached = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.countryName),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            // "더이상 누를 수 없습니다!" 문구
-            Visibility(
-              visible: isMaxSizeReached,
-              child: SizedBox(
-                height: 30.0,
-                child: Center(
-                  child: Text(
-                    "더이상 누를 수 없습니다!",
-                    style: TextStyle(color: Colors.red),
-                  ),
-                ),
-              ),
-            ),
-            // 좋아요 아이콘
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Image.asset(
-                'lib/icons/heart.png',
-                height: iconSize,
-                width: iconSize,
-              ),
-            ),
-            SizedBox(height: 16.0),
-
-            // "+" 버튼
-            ElevatedButton(
+  void _showLogoutDialog(BuildContext context, String username) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Logged in as $username'),
+          content: Text('Do you want to log out?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Logout'),
               onPressed: () {
-                if (iconSize + 10.0 <= 530.0) {
-                  setState(() {
-                    iconSize += 10.0;
-                    isMaxSizeReached = false;
-                  });
-                } else {
-                  setState(() {
-                    isMaxSizeReached = true;
-                  });
-
-                  Future.delayed(Duration(seconds: 1), () {
-                    setState(() {
-                      isMaxSizeReached = false;
-                    });
-                  });
-                }
+                ref.read(userProvider.state).state = null;
+                Navigator.of(context).pop();
               },
-              child: Text("+"),
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(50, 50),
-              ),
             ),
-            SizedBox(height: 16.0),
-
-            // "-" 버튼
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  iconSize -= 10.0;
-                });
-              },
-              child: Text("-"),
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(50, 50),
-              ),
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(),
             ),
           ],
-        ),
+        );
+      },
+    );
+  }
+
+  void _showLoginDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => Dialog(
+        child: LoginScreen(),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
       ),
     );
   }
