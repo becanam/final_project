5bdb9bae75cc86b6057fc97c2a2c1a729eab7b06
junghyun <mattew423@gmail.com>
2023-12-24 19:22:36 +0900

    trying to fix homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,139 +1,154 @@
 import 'package:flutter/material.dart';
-import 'package:midterm_project/pages/details_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:midterm_project/providers/user_provider.dart';
+import 'package:midterm_project/utilities/login.dart';
 import 'package:midterm_project/utilities/navigation_bar.dart';
-import 'package:midterm_project/utilities/travelcards_list.dart';
-import 'package:midterm_project/utilities/travel_options.dart';
 
-class Homepage extends StatefulWidget {
-  const Homepage({super.key});
+class Homepage extends ConsumerStatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
 
   @override
-  State<Homepage> createState() => _HomePageState();
+  _HomepageState createState() => _HomepageState();
 }
 
-class _HomePageState extends State<Homepage> {
-  //padding constants
-  final double horizontalPadding = 30;
-  final double verticalPadding = 20;
+class _HomepageState extends ConsumerState<Homepage> {
+  Future<void> fetchUserData() async {
+    var url = Uri.parse('http://localhost:3000/users');
+    try {
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        var users = json.decode(response.body) as List;
+        if (users.isNotEmpty) {
+          var currentUser = users.last['username'];
+          ref.read(userProvider.state).state = currentUser;
+        } else {
+          ref.read(userProvider.state).state = null;
+        }
+      } else {
+        print('Failed to fetch users: ${response.statusCode}, ${response.body}');
+      }
+    } catch (e) {
+      print('Error occurred while fetching users: $e');
+    }
+  }
+
+  Future<List<dynamic>> fetchTravelPlans() async {
+    var url = Uri.parse('http://localhost:3000/travel_plans');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      print('Failed to fetch travel plans: ${response.statusCode}, ${response.body}');
+      return [];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    final username = ref.watch(userProvider.state).state;
+    final double horizontalPadding = 30;
+    final double verticalPadding = 20;
+
     return Scaffold(
-        bottomNavigationBar: const BottomNavBar(),
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              //appbar
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: horizontalPadding,
-                  vertical: verticalPadding,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    //menu icon
-                    CircleAvatar(
-                      backgroundColor: Colors.grey[300],
-                      radius: 20,
-                      child: Image.asset('lib/icons/app.png',
-                          height: 20, color: Colors.grey[800]),
+      bottomNavigationBar: const BottomNavBar(),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // ... Existing AppBar and padding code
+
+            Expanded(
+              child: FutureBuilder<List<dynamic>>(
+                future: fetchTravelPlans(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(child: Text("No travel plans available."));
+                  }
+                  return GridView.builder(
+                    itemCount: snapshot.data!.length,
+                    padding: const EdgeInsets.all(16),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1 / 1.2,
                     ),
-
-                    //profile
-                    CircleAvatar(
-                      backgroundColor: Colors.grey[300],
-                      radius: 20,
-                      child: Image.asset('lib/icons/user.png',
-                          height: 20, color: Colors.grey[800]),
-                    )
-                  ],
-                ),
-              ),
-
-              //title
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: horizontalPadding, vertical: verticalPadding),
-                child: Container(
-                    width: 250,
-                    height: 105,
-                    color: Colors.black.withOpacity(0),
-                    child: const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Best Places For Travel",
-                          style: TextStyle(
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold,
+                    itemBuilder: (context, index) {
+                      var plan = snapshot.data![index];
+                      return GestureDetector(
+                        onTap: () {
+                          // Define action on tap, if needed
+                        },
+                        child: Card(
+                          elevation: 5, // Add elevation for depth
+                          margin: EdgeInsets.all(8), // Margin for spacing
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10), // Rounded corners
+                          ),
+                          clipBehavior: Clip.antiAlias,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                              Expanded(
+                                child: Image.network(
+                                  plan['imageUrl'], // Replace with actual image URL property from your plan
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      plan['name'], // Replace with actual title property from your plan
+                                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                      textAlign: TextAlign.start,
+                                    ),
+                                    SizedBox(height: 5),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        Text(
-                          "Find the best places to visit!",
-                          style: TextStyle(
-                              wordSpacing: 2,
-                              height: 2,
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromARGB(255, 135, 131, 131)),
-                        ),
-                      ],
-                    )),
+                      );
+                    },
+                  );
+                },
               ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: horizontalPadding, vertical: verticalPadding),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    //trending now
-                    Text(
-                      "Trending Now",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-
-                    //see all
-                    Text(
-                      "See all",
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-
-              //travel options
-              Expanded(
-                child: GridView.builder(
-                  itemCount: travelOptionsList.length,
-                  padding: const EdgeInsets.all(16),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 1 / 1.65,
-                  ),
-                  itemBuilder: (context, index) {
-                    return PlacesToTravel(
-                        travelCard: travelOptionsList[index],
-                        onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => DetailsScreen(
-                                    travelCard: travelOptionsList[index]))));
-                  },
-                ),
-              ),
-            ],
-          ),
-        ));
+  void _showLogoutDialog(BuildContext context, String username) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Logged in as $username'),
+          content: Text('Do you want to log out?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Logout'),
+              onPressed: () {
+                ref.read(userProvider.state).state = null;
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
