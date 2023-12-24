4ac2eee89aeedd0ef952e25c1b078da5a88d500c
junghyun <mattew423@gmail.com>
2023-12-24 22:59:23 +0900

    ddd

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -36,28 +36,26 @@ class _HomepageState extends ConsumerState<Homepage> {
     }
   }
 
- void animateCounts(int planId, String type) {
-  final travelPlans = ref.read(travelPlanProvider);
-  final List<Map<String, dynamic>> updatedPlans = List.from(travelPlans);
+  
 
-  final planIndex = updatedPlans.indexWhere((plan) => plan['id'] == planId);
-  final plan = updatedPlans[planIndex];
+  void animateCounts(int planId, String type) {
+    final travelPlans = ref.read(travelPlanProvider);
 
-  final likes = plan['likes'] ?? 0;
-  final recommendations = plan['recommendations'] ?? 0;
+    final planIndex = travelPlans.indexWhere((plan) => plan['id'] == planId);
+    final plan = travelPlans[planIndex];
 
-  final newLikes = type == 'like' ? likes + 1 : likes;
-  final newRecommendations = type == 'recommendation' ? recommendations + 1 : recommendations;
+    final likes = plan['likes'] ?? 0;
+    final recommendations = plan['recommendations'] ?? 0;
 
-  updatedPlans[planIndex] = {
-    ...plan,
-    'likes': newLikes,
-    'recommendations': newRecommendations,
-  };
+    final newLikes = type == 'like' ? likes + 1 : likes;
+    final newRecommendations = type == 'recommendation' ? recommendations + 1 : recommendations;
 
-  ref.read(travelPlanProvider).state = updatedPlans;
+    travelPlans[planIndex] = {
+      ...plan,
+      'likes': newLikes,
+      'recommendations': newRecommendations,
+    };
 }
-
 
 
   Future<List<dynamic>> fetchTravelPlans() async {
@@ -71,25 +69,54 @@ class _HomepageState extends ConsumerState<Homepage> {
     }
   }
 
-  Future<void> incrementLikes(int planId) async {
-    var url = Uri.parse('http://localhost:3000/travel_plans/$planId/likes');
-    var response = await http.patch(url);
-    if (response.statusCode != 200) {
-      print('Failed to increment like: ${response.statusCode}, ${response.body}');
-    } else {
-      animateCounts(planId, 'like');
-    }
-  }
 
-  Future<void> incrementRecommendations(int planId) async {
-    var url = Uri.parse('http://localhost:3000/travel_plans/$planId/recommendations');
-    var response = await http.patch(url);
-    if (response.statusCode != 200) {
-      print('Failed to increment recommendation: ${response.statusCode}, ${response.body}');
-    } else {
-      animateCounts(planId, 'recommendation');
-    }
+  
+Future<void> incrementLikes(Map<String, dynamic> plan) async {
+  var url = Uri.parse('http://localhost:3000/travel_plans/${plan['id']}');
+  int currentLikes = plan['likes'] ?? 0;
+  var response = await http.patch(
+    url,
+    headers: {"Content-Type": "application/json"},
+    body: json.encode({
+      'likes': currentLikes + 1, // Increment like count
+      // Include other plan properties if required by your backend
+    }),
+  );
+
+  if (response.statusCode != 200) {
+    print('Failed to increment like: ${response.statusCode}, ${response.body}');
+  } else {
+    // Update local state with new likes count
+    setState(() {
+      plan['likes'] = currentLikes + 1;
+    });
   }
+}
+
+
+
+
+ Future<void> incrementRecommendations(Map<String, dynamic> plan) async {
+  var url = Uri.parse('http://localhost:3000/travel_plans/${plan['id']}');
+  int currentRecommendations = plan['recommendations'] ?? 0;
+  var response = await http.patch(
+    url,
+    headers: {"Content-Type": "application/json"},
+    body: json.encode({
+      'recommendations': currentRecommendations + 1, // Increment recommendations count
+      // Include other plan properties if required by your backend
+    }),
+  );
+
+  if (response.statusCode != 200) {
+    print('Failed to increment recommendation: ${response.statusCode}, ${response.body}');
+  } else {
+    // Update local state with new recommendations count
+    setState(() {
+      plan['recommendations'] = currentRecommendations + 1;
+    });
+  }
+}
 
 
   @override
@@ -163,69 +190,72 @@ class _HomepageState extends ConsumerState<Homepage> {
                   if (!snapshot.hasData || snapshot.data!.isEmpty) {
                     return Center(child: Text("No travel plans available."));
                   }
-                  return GridView.builder(
-                    itemCount: snapshot.data!.length,
-                    padding: const EdgeInsets.all(16),
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 1 / 0.8,
-                    ),
-                    itemBuilder: (context, index) {
-                      var plan = snapshot.data![index];
-                      final likeTween = Tween<double>(begin: 1, end: 1 + 0.5 * (plan['likes'] ?? 0));
-                      final recommendationTween = Tween<double>(begin: 1, end: 1 + 0.1 * (plan['recommendations'] ?? 0));
-                      return GestureDetector(
-                        onTap: () {
-                          // Define action on tap, if needed
-                        },
-                        child: Card(
-                          clipBehavior: Clip.antiAlias,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: <Widget>[
-                              Image.network(
-                                plan['imageUrl'], // Replace with actual image URL property from your plan
-                                height: 250.0,
-                                fit: BoxFit.cover,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(12.0),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    Text(
-                                      plan['name'], // Replace with actual title property from your plan
-                                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                                      textAlign: TextAlign.center,
-                                    ),
-                                   Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                 return GridView.builder(
+    itemCount: snapshot.data!.length,
+    padding: const EdgeInsets.all(16),
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2,
+      childAspectRatio: 1 / 0.8,
+    ),
+    itemBuilder: (context, index) {
+      var plan = snapshot.data![index];
+
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.network(
+              plan['imageUrl'],
+              height: 250.0,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    plan['name'],
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 TweenAnimationBuilder<double>(
-                  tween: likeTween,
-                  duration: Duration(seconds: 1),
+                  tween: Tween<double>(begin: 1.0, end: 1.0 + 0.05 * (plan['likes'] ?? 0)), // Adjust these values as needed
+                  duration: Duration(milliseconds: 1500),
+                  curve: Curves.elasticOut,
                   builder: (context, scale, child) {
-                    return Transform.scale(
-                      scale: scale,
-                      child: IconButton(
-                        icon: Icon(Icons.favorite),
-                        color: Colors.red,
-                        onPressed: () => incrementLikes(plan['id']),
+                    return Transform.translate(
+                      offset: Offset(scale * 5, 0), // This creates the side to side movement
+                      child: Transform.scale(
+                        scale: scale,
+                        child: IconButton(
+                          icon: Icon(Icons.favorite),
+                          color: Colors.red,
+                          onPressed: () => incrementLikes(plan),
+                        ),
                       ),
                     );
                   },
                 ),
                 Text('${plan['likes'] ?? 0} likes'),
                 TweenAnimationBuilder<double>(
-                  tween: recommendationTween,
-                  duration: Duration(seconds: 1),
+                  tween: Tween<double>(begin: 1.0, end: 1.0 + 0.05 * (plan['likes'] ?? 0)), // Adjust these values as needed
+                  duration: Duration(milliseconds: 1500),
+                  curve: Curves.elasticOut,
                   builder: (context, scale, child) {
-                    return Transform.scale(
-                      scale: scale,
-                      child: IconButton(
-                        icon: Icon(Icons.thumb_up),
-                        color: Colors.blue,
-                        onPressed: () => incrementRecommendations(plan['id']),
+                    return Transform.translate(
+                      offset: Offset(scale * 5, 0), // This creates the side to side movement
+                      child: Transform.scale(
+                        scale: scale,
+                        child: IconButton(
+                          icon: Icon(Icons.thumb_up),
+                          color: Colors.blue,
+                          onPressed: () => incrementRecommendations(plan),
+                        ),
                       ),
                     );
                   },
@@ -233,16 +263,15 @@ class _HomepageState extends ConsumerState<Homepage> {
                 Text('${plan['recommendations'] ?? 0} recommendations'),
               ],
             ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
+          ],
+        ),
+      ),
+    ],
+  ),
+);
+    },
+  );
+}
               ),
             ),
           ],
